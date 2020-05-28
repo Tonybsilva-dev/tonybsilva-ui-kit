2a5697fba95b1ebbfa733ad52aecd82cb5daff9c
Antônio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-05-27 23:54:15 -0300

    Part 1

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+import {} from '@storybook/theming';
+
+export default({
+  base: 'dark',
+
+  colorSecondary: '#7159c1',
+
+  appBg: '#0b0a0d',
+  appContentBg: '#16161A',
+  appBorderColor: '#7159c1',
+
+  textColor: '#fff',
+
+  //ToolBar default and active colors
+  barTextColor: '#E6E6E6',
+  barSelectedColor: '#ffffff',
+  barBg: '#1b1a1f',
+
+  brandTitle: 'RunTime Ui',
+  brandUrl: 'https://github.com/yRunTimeException',
+});
